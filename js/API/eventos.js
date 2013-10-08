e34bc4abb47379059b0c69a3f8ea8ee590f926a2
Sergio Serrano Perez <pepes17@hotmail.com>
2013-10-08 10:08:44 -0500

    asas

--- js/API/eventos.js
+++ js/API/eventos.js
@@ -9,7 +9,7 @@ $(document).ready(function (e) {
 
 		window.location.href = '#form1';
 
-		
+
 
 		//---------------datos 
 		$('#datos').tap(function () {
@@ -18,7 +18,7 @@ $(document).ready(function (e) {
 			obtener_clave(cla);
 
 
-		
+
 
 		});
 
@@ -27,13 +27,13 @@ $(document).ready(function (e) {
 			window.location.reload();
 
 
-	
+
 
 		});
 		//---------------guarda encuesta
 
 		$('#guarda').tap(function () {
-			var exito=true;
+			var exito = true;
 			var check = getCookie("indice");
 			if (check != null && check != "") {
 
@@ -49,174 +49,150 @@ $(document).ready(function (e) {
 			var valnombre = $('#nombre').val();
 			if (!valnombre) {
 				alert("Favor de escribir tu nombre");
-				exito= false;
+				exito = false;
 			}
-			
+
 			var valdireccion = $('#direccion').val();
 			if (!valdireccion) {
 				alert("Favor de escribir tu direccion");
-				exito= false;
+				exito = false;
 			}
 			var valciudad = $('#ciudad').val();
 			if (!valciudad) {
 				alert("Favor de escribir tu ciudad");
-				exito= false;
+				exito = false;
 			}
-			
-			
-			var valestado= $('#estado').val();
+
+
+			var valestado = $('#estado').val();
 			if (!valestado) {
 				alert("Favor de escribir tu ciudad");
-				exito= false;
+				exito = false;
 			}
-		
-			
+
+
 			var valphone = $('#telefono').val();
 			if (!valphone) {
 				alert("Favor de escribir tu numero telefonico");
-				exito= false;
+				exito = false;
 			}
 			var valemail = $('#email').val();
 			if (!valemail) {
 				alert("Favor de escribir tu email");
-				exito= false;
+				exito = false;
 			}
-			
+
 			var valcp = $('#cp').val();
 			if (!valcp) {
-				valcp=" ";
+				valcp = " ";
 			}
-			
+
 			var valempresa = $('#empresa').val();
 			if (!valempresa) {
 				alert("Favor de escribir tu email");
-				exito= false;
+				exito = false;
 			}
-			
+
 			var valpuesto = $('#puesto').val();
 			if (!valpuesto) {
-				valpuesto=" ";
+				valpuesto = " ";
 			}
-		// inter3es
-			var valcat=0;
-			var check_cat =  $("#checkbox1_0").is(":checked");
-			if (check_cat)
-			{
-			 valcat=1;
+			// inter3es
+			var valcat = 0;
+			var check_cat = $("#checkbox1_0").is(":checked");
+			if (check_cat) {
+				valcat = 1;
 			}
-		
+
 			if (!check_cat) {
-				valcat=0;
-				exito= false;
+				valcat = 0;
+				exito = false;
 			}
-			var valpro=0;
-			var check_pro =  $("#checkbox1_1").is(":checked");
-			if (check_pro)
-			{
-			r valpro=1;
+			var valpro = 0;
+			var check_pro = $("#checkbox1_1").is(":checked");
+			if (check_pro) {
+				valpro = 1;
 			}
-		
+
 			if (!check_pro) {
-				valpro=0;
-				exito= false;
+				valpro = 0;
+				exito = false;
 			}
-			var valinv=0;
-			var check_inv =  $("#checkbox1_2").is(":checked");
-			if (check_inv)
-			{
-			 valinv=1;
+			var valinv = 0;
+			var check_inv = $("#checkbox1_2").is(":checked");
+			if (check_inv) {
+				valinv = 1;
 			}
-		
+
 			if (!check_inv) {
-				valinv=0;
-				exito= false;
+				valinv = 0;
+				exito = false;
 			}
-			
-			
+
+
 			//lineas
-			
-			var valvx=0;
-			var check_vx =  $("#checkbox1_2").is(":checked");
-			if (check_vx)
-			{
-			 valvx=1;
+
+			var valvx = 0;
+			var check_vx = $("#checkbox1_2").is(":checked");
+			if (check_vx) {
+				valvx = 1;
 			}
-		
+
 			if (!check_vx) {
-				valvx=0;
-				exito= false;
+				valvx = 0;
+				exito = false;
 			}
-			var valmgl=0;
-			var check_mgl =  $("#checkbox1_2").is(":checked");
-			if (check_mgl)
-			{
-			 valmgl=1;
+			var valmgl = 0;
+			var check_mgl = $("#checkbox1_2").is(":checked");
+			if (check_mgl) {
+				valmgl = 1;
 			}
-		
+
 			if (!check_mgl) {
-				valmgl=0;
-				exito= false;
+				valmgl = 0;
+				exito = false;
 			}
-			var valcel=0;
-			var check_cel =  $("#checkbox1_2").is(":checked");
-			if (check_cel)
-			{
-			 valcel=1;
+			var valcel = 0;
+			var check_cel = $("#checkbox1_2").is(":checked");
+			if (check_cel) {
+				valcel = 1;
 			}
-		
+
 			if (!check_cel) {
-				valcel=0;
-				exito= false;
+				valcel = 0;
+				exito = false;
 			}
-			var valfel=0;
-			var check_fel =  $("#checkbox1_2").is(":checked");
-			if (check_fel)
-			{
-			 valfel=1;
+			var valfel = 0;
+			var check_fel = $("#checkbox1_2").is(":checked");
+			if (check_fel) {
+				valfel = 1;
 			}
-		
+
 			if (!check_fel) {
-				valfel=0;
-				exito= false;
+				valfel = 0;
+				exito = false;
 			}
-			
-			var valwen=0;
-			var check_wen =  $("#checkbox1_2").is(":checked");
-			if (check_wen)
-			{
-			 valwen=1;
+
+			var valwen = 0;
+			var check_wen = $("#checkbox1_2").is(":checked");
+			if (check_wen) {
+				valwen = 1;
 			}
-		
+
 			if (!check_wen) {
-				valwen=0;
-				exito= false;
+				valwen = 0;
+				exito = false;
 			}
-			
-			
-		
-			
-			/*var valrad3 = $('input:radio[name=pregunta3]:checked').val();
-			if (!valrad3) {
-				alert("Please select your option on pregunta3.");
-				exito= false;
-			}
-			var valrad4 = $('input:radio[name=pregunta4]:checked').val();
-			if (!valrad4) {
-				alert("Please select your option on pregunta4.");
-				exito= false;
-			}
-			var valrad5 = $('input:radio[name=pregunta5]:checked').val();
-			if (!valrad5) {
-				alert("Please select your option on pregunta 5.");
-				exito= false;
-			}*/
 
 
-			if (exito)
-			{
+
+
+
+
+
 			if (isConnected()) {
 
-				var msg = guarda_cliente(valnombre, valdireccion, valciudad, valestado, valphone,valemail,valempresa,valpuesto,valcp,valcat,valpro,valinv,valvx,valmgl,valcel,valfel,valwen, cont);
+				var msg = guarda_cliente(valnombre, valdireccion, valciudad, valestado, valphone, valemail, valempresa, valpuesto, valcp, valcat, valpro, valinv, valvx, valmgl, valcel, valfel, valwen, cont);
 				var cont2 = parseInt(cont) + 1;
 				cont = cont2.toString();
 				setCookie("indice", cont, 365);
@@ -224,16 +200,14 @@ $(document).ready(function (e) {
 
 				leeresarvas2();
 			} else {
-				guardatempcalif(valnombre, valdireccion, valciudad, valestado, valphone,valemail,valempresa,valpuesto,valcp,valcat,valpro,valinv,valvx,valmgl,valcel,valfel,valwen);
+				guardatempcalif(valnombre, valdireccion, valciudad, valestado, valphone, valemail, valempresa, valpuesto, valcp, valcat, valpro, valinv, valvx, valmgl, valcel, valfel, valwen);
 			}
-			}
+
 			return false;
 
-			
+
 
 
 		});
 	});
 });
-
-	
