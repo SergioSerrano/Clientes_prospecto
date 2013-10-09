8fa3696559c2d9d2a52797dcb15db18e0ec3946b
Sergio Serrano Perez <pepes17@hotmail.com>
2013-10-09 09:36:11 -0500

    mensaje

--- js/API/servidor.js
+++ js/API/servidor.js
@@ -72,7 +72,7 @@ function guarda_calif(valrad1,valrad2,valrad3,valrad4,valrad5,times){
 }
              
 function guarda_cliente(val1, val2, val3, val4, val5, val6, val7, val8, val9, val10, val11, val12, val13, val14, val15, val16, val17, times) {
-
+alert( "c1="+val1+"&c2="+val2+"&c3="+val3+"&c4="+val4+"&c5="+val5+"&times="+times);
 	$.ajax({
 		type: "POST",
 		url: "http://testapp2.260mb.net/sincronizar/g_preguntas.php",
