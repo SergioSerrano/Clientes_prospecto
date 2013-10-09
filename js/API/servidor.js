33620b05b8c72b1964647bbd9ab67cae5f3fd845
Sergio Serrano Perez <pepes17@hotmail.com>
2013-10-09 10:24:37 -0500

    crear ajax

--- js/API/servidor.js
+++ js/API/servidor.js
@@ -1,4 +1,26 @@
 //Servidor
+function nuevoAjax()
+{
+        var xmlhttp=false;
+        try
+        {
+                // Creacion del objeto AJAX para navegadores no IE
+                xmlhttp=new ActiveXObject("Msxml2.XMLHTTP");
+        }
+        catch(e)
+        {
+                try
+                {
+                        // Creacion del objet AJAX para IE
+                        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+                }
+                catch(E)
+                {
+                        if (!xmlhttp && typeof XMLHttpRequest!="undefined") xmlhttp=new XMLHttpRequest();
+                }
+        }
+        return xmlhttp;
+}
 function enviarDatos(nom, tel, email){
 	$.ajax({
 		type: "POST",
